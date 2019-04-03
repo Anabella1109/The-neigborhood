d7a58a35903da9c81ec55bb062a02095497478d5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-04-03 18:01:08 +0200

    add post model class in models.py

--- theNeighborhood/models.py
+++ theNeighborhood/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
 class Business(models.Model):
      name=models.CharField(max_length=100,null=True)
      location=models.CharField(max_length=200,null=True)
-     neigborhood=models.OneToOneField(Neigborhood,null=True)
+     neighborhood=models.OneToOneField(Neighborhood,null=True)
     #  user=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
      email = models.EmailField()
      phone_number=models.IntegerField(null=True)
@@ -21,17 +21,24 @@ class Business(models.Model):
 class Health(models.Model):
     name=models.CharField(max_length=100,null=True)
     location=models.CharField(max_length=200,null=True)
-    neigborhood=models.OneToOneField(Neigborhood,null=True)
+    neighborhood=models.OneToOneField(Neighborhood,null=True)
     phone_number=models.IntegerField(null=True)
     email = models.EmailField()
 
 class Police(models.Model):
     name=models.CharField(max_length=100,null=True)
     location=models.CharField(max_length=200,null=True)
-    neigborhood=models.OneToOneField(Neigborhood,null=True)
+    neighborhood=models.OneToOneField(Neighborhood,null=True)
     phone_number=models.IntegerField(null=True)
     email = models.EmailField()
 
+class Post(models.Model):
+    name=models.CharField(max_length=100,null=True)
+    image=models.ImageField(upload_to = 'images/')
+    caption=HTMLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    profile=models.ForeignKey(Profile, null=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
 
      
-# Create your models here.
