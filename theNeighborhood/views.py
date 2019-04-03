8863a5b9957e8d645dfc2b839943ae2805c8a2b5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-04-03 16:07:01 +0200

    create view function to view home page

--- theNeighborhood/views.py
+++ theNeighborhood/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+
+
+def home(request):
+    return render(request,'home.html')
 
 # Create your views here.
