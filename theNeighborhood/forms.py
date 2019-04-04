2f08009810ab33d07f0dfaf6d3bbaf81e374e5b7
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-04-04 14:57:58 +0200

    create form in forms.py and view function in views.py to edit profile

--- theNeighborhood/forms.py
+++ theNeighborhood/forms.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User,Profile
  
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email','password')
+
+class Profileform(forms.ModelForm):
+     class Meta:
+         model= Profile
+         exclude = ['user']
--- theNeighborhood/views.py
+++ theNeighborhood/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm, Profileform
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 
 
@@ -8,17 +8,28 @@ def home(request):
     return render(request,'home/home.html')
 
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(email=user.email, password=raw_password)
-#             login(request, user)
-#             return redirect('home')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'signup.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request,edit):
+    current_user = request.user
+    profile=Profile.objects.get(user=current_user)
+    
+   
+    if request.method == 'POST':
+        form = Profileform(request.POST, request.FILES)
+        if form.is_valid():
+            
+            profile.bio=form.cleaned_data['bio']
+            profile.photo = form.cleaned_data['photo']
+            profile.user=current_user
+            
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = Profileform()
+    return render(request, 'edit_profile.html', {"form": form , 'user':current_user})
+
+
 
 # Create your views here.
