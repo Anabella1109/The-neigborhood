54f9b2d90c0755a29d53f1cc0c7b37e7d317db99
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-04-04 14:46:59 +0200

    register all the model class to the admin site

--- theNeighborhood/admin.py
+++ theNeighborhood/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, Business,Neighborhood, Police , Health, Post
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Police)
+admin.site.register(Health)
+admin.site.register(Post)
 
 # Register your models here.
--- theNeighborhood/models.py
+++ theNeighborhood/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tinymce.models import HTMLField
-
+from django.contrib.auth.models import User
 class Neighborhood(models.Model):
      name = models.CharField(max_length=100, null=True)
      location = models.CharField(max_length=100, null=True)
