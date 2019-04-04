2fbf7288397d0e5ec9c03d2f3f47d94e18e72a0f
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-04-04 15:09:53 +0200

    add template to view profile(profile.html) and view function

--- theNeighborhood/forms.py
+++ theNeighborhood/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import User,Profile
+from django import forms
  
 class SignUpForm(UserCreationForm):
     class Meta:
--- theNeighborhood/views.py
+++ theNeighborhood/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import SignUpForm, Profileform
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
-
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib.auth.models import User
 
 def home(request):
-    return render(request,'home/home.html')
+    return render(request, 'home/home.html')
 
 
 
@@ -21,7 +23,11 @@ def edit_profile(request,edit):
             
             profile.bio=form.cleaned_data['bio']
             profile.photo = form.cleaned_data['photo']
+            profile.first_name = form.cleaned_data['first_name']
+            profile.last_name = form.cleaned_data['last_name']
+            profile.phone_number = form.cleaned_data['phone_number']
             profile.user=current_user
+
             
             profile.save()
         return redirect('home')
@@ -29,6 +35,15 @@ def edit_profile(request,edit):
     else:
         form = Profileform()
     return render(request, 'edit_profile.html', {"form": form , 'user':current_user})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+     user=User.objects.get(id=id)
+     profile=Profile.objects.get(user=user)
+     
+     
+     return render(request, 'profile.html',{"user":user,"profile": profile})
+     
 
 
 
